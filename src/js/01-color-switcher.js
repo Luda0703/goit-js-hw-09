e80420b9ed3e1e04712508b55e906bde68f4b108
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-04-04 17:50:23 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
 
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const dataBody = document.querySelector('body');
+
+dataStart.addEventListener('click', onStartColor);
+dataStop.addEventListener('click', onStopColor);
+
+let timerId = null;
+ let isActive = true;
+
+function onStartColor() {
+    timerId = setInterval(getBgColor, 1000);
+    // if(!isActive) {
+    //     dataStart.toggleAttribute('disabled');
+    // } else {
+    //     delete(dataStart.toggleAttribute('disabled'));
+    // }
+    dataStart.disabled = true
+    if(dataStart.disabled) {
+        dataStop.disabled = false
+    }
+
+}
+
+function onStopColor() {
+    clearInterval(timerId);
+    // if(!isActive) {
+    //     dataStop.toggleAttribute('disabled');
+    // } else {
+    //     delete(dataStart.toggleAttribute('disabled'));
+    // }
+    dataStop.disabled = true
+    if(dataStop.disabled) {
+        dataStart.disabled = false
+    }
+}
+
+function getBgColor() {
+    dataBody.style.backgroundColor = getRandomHexColor();
+} 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)
+    }`;
+  }
+
+ 
+  
